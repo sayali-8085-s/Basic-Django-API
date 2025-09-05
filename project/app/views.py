048ceeeb2d320039d9667f90ceec2edcfea4b5d4
sayali singh <sayalibaghel6@gmail.com>
2025-09-05 21:32:36 +0530

    n

--- project/app/views.py
+++ project/app/views.py
@@ -1,26 +1,84 @@
+# from django.shortcuts import render
+# from django.http import JsonResponse ,HttpResponse
+# from .models import Student
+# from .serializers import StudentSerializer
+# import json
+# from django.views.decorators.csrf import csrf_exempt
+
+# # Create your views here.
+# csrf_exempt
+
+
+# def stu_list(req):
+#     if req.method == 'POST':
+#         print('post')
+#         print(req.body)
+#         data = json.loads(req.body)
+#         print(data)
+#         print(type(data))
+#         Student.object.create(name =data['name'] ,age = data['age'] ,email =data['email'])
+#         print('data save succeesssssssss')
+        
+#     else:    
+#      x =Student.objects.all()
+#      s =  StudentSerializer(x,many =True)
+#     # print(s)
+#     # print(s.data)
+#     # return JsonResponse(s.data ,safe=False)
+#      jdata=json.dumps(s.data)
+#      print(jdata)
+#      return HttpResponse(jdata,content_type ='application/json')
+
+
+# def stu_detail(req ,pk):
+#     x =Student.objects.get(id = pk)
+#     s =  StudentSerializer(x)
+#     # print(s)
+#     # print(s.data)
+#     return JsonResponse(s.data ,safe=False)
+
+
 from django.shortcuts import render
-from django.http import JsonResponse ,HttpResponse
+from django.http import JsonResponse, HttpResponse
 from .models import Student
 from .serializers import StudentSerializer
 import json
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-
+@csrf_exempt   # 👈 yaha decorator lagana zaroori hai
 def stu_list(req):
-    x =Student.objects.all()
-    s =  StudentSerializer(x,many =True)
-    # print(s)
-    # print(s.data)
-    # return JsonResponse(s.data ,safe=False)
-    jdata=json.dumps(s.data)
-    print(jdata)
-    return HttpResponse(jdata,content_type ='application/json')
+    if req.method == 'POST':
+        print('post')
+        print(req.body)
+        
+        # body se JSON data nikaalo
+        data = json.loads(req.body)
+        print(data)
+        print(type(data))
+        
+        # student create karo
+        Student.objects.create(
+            name=data['name'],
+            age=data['age'],
+            email=data['email'],
+            roll_no=data['roll_no'],   # required field
+            grade=data.get('grade')    # optional
+        )
+        print('data save succeesssssssss')
+        
+        # success response bhejna zaruri hai
+        return JsonResponse({"message": "Student created successfully!"})
+
+    else:
+        x = Student.objects.all()
+        s = StudentSerializer(x, many=True)
+        return JsonResponse(s.data, safe=False)
 
 
-def stu_detail(req ,pk):
-    x =Student.objects.all(id = pk)
-    s =  StudentSerializer(x)
-    # print(s)
-    # print(s.data)
-    return JsonResponse(s.data ,safe=False)
+@csrf_exempt   # 👈 detail view pe bhi lagana padega agar POST/PUT karna hai
+def stu_detail(req, pk):
+    x = Student.objects.get(id=pk)
+    s = StudentSerializer(x)
+    return JsonResponse(s.data, safe=False)
